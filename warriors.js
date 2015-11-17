2385e755f28b27c6f4a3c5603bea7641bcd10bfa
babakkhosravifar <babak.khosravifar@gmail.com>
2015-11-17 14:55:37 +0000

    Warriors Started

--- warriors.js
+++ warriors.js
@@ -1,1 +1,30 @@
 // one line comment
+
+function makeObjects(name, gender,opponent){
+var numberObject= {
+    name: name,
+    gender: gender,
+    level: 1,
+    weapon: "wooden sword",
+    power: Math.floor(Math.random() * 100) + 1,
+    fight: function(){
+        return "rushes to the arena with ";
+    },
+    faceoff: function(opponent){
+        if(this.power>opponent.power)
+           return this.name + " killed " + opponent.name;
+        else
+            return opponent.name + " killed " + this.name;
+    }
+};
+return numberObject;
+}
+
+var objects = [];
+objects.push(makeObjects("Babak", "M","Lauren"));
+objects.push(makeObjects("Ziad", "M","Nora"));
+objects.push(makeObjects("Nora", "M","Babak"));
+objects.push(makeObjects("Laurren", "F","Ziad"));
+console.log(objects[0].fight());
+console.log(objects[0].faceoff(objects[1]));
+//console.log(objects);
